5358815825dae0cf334d5e0248792fd2a9323bc0
Fran <frangal@gmail.com>
2021-12-06 16:04:06 +0100

    not retry fail

--- msxvr-builder.sh
+++ msxvr-builder.sh
@@ -67,7 +67,7 @@ fi
 installdeps() {
   [ $APT_UPDATE = "0" ] && apt-get update; APT_UPDATE="1"
   apt-get -q -y install --no-install-recommends -o APT::Install-Suggests=0 \
-      -o dpkg::options::=--force-confnew -o Acquire::Retries=3 "$DEPS" || installdeps
+      -o dpkg::options::=--force-confnew -o Acquire::Retries=3 $DEPS
 }
 
 # Instalar dependencias necesarias
